e8c25350bb34774325616c8690248b00a32e0651
João Francisco <joaofrancisco.amello@gmail.com>
2026-08-23 23:02:26 -0300

    adiciona o código do curso 3 da carreira de node.js da alura

---
+++ nivel-1/curso-03/src/controllers/autoresController.js
@@ -0,0 +1,69 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
+import { autores } from "../models/index.js";
+
+class AutorController {
+
+  static listarAutores = async(req, res, next) => {
+    try {
+      const autoresResultado = autores.find();
+      req.resultado = autoresResultado;
+      next();
+    } catch (erro) {
+        res.status(500).json({ message: erro.message });
+    }
+  }
+
+  static listarAutorPorId = async (req, res, next) => {
+      try {
+        const id = req.params.id;
+  
+        const autorResultado = await autores.findById(id);
+        if (autorResultado !== null) {
+          res.status(200).send(autorResultado);
+        } else {
+          next(new NaoEncontrado("Id do autor não localizado"));
+        }
+      } catch (erro) {
+        next(erro);
+      }
+    }
+  
+    static cadastrarAutor = async (req, res, next) => {
+      try {
+        let autor = new autores(req.body);
+  
+        const autorResultado = await autor.save();
+  
+        res.status(201).send(autorResultado.toJSON());
+      } catch (erro) {
+        next(erro);
+      }
+    }
+
+    static atualizarAutor = async (req, res, next) => {
+      try {
+        const id = req.params.id;
+  
+        await autores.findByIdAndUpdate(id, {$set: req.body});
+  
+        res.status(200).send({message: "Autor atualizado com sucesso"});
+      } catch (erro) {
+        next(erro);
+      }
+    }
+  
+    static excluirAutor = async (req, res, next) => {
+      try {
+        const id = req.params.id;
+  
+        await autores.findByIdAndDelete(id);
+  
+        res.status(200).send({message: "Autor removido com sucesso"});
+      } catch (erro) {
+        next(erro);
+      }
+    }
+    
+}
+
+export default AutorController
